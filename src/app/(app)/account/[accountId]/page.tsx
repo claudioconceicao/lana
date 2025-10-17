e546d2fd8754fa1a8fe4b4c849a072fedafa175f
Claudio Conceicao <mvrtivs@protonmail.com>
2025-10-17 14:39:30 +0100

    UPDA

--- src/app/(app)/account/[accountId]/page.tsx
+++ src/app/(app)/account/[accountId]/page.tsx
@@ -1,4 +1,12 @@
-"use client";
+export async function generateStaticParams() {
+  // Replace with your own data or hardcoded IDs
+  return [
+    { accountId: "1" },
+    { accountId: "2" },
+    { accountId: "3" },
+  ];
+}
+
 import { useSession } from "@/context/SessionContext";
 import { redirect, RedirectType } from "next/navigation";
 
